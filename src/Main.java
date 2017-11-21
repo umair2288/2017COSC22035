e4a3c8f97a25e47f805ec6d1bc0309f12259748a
Cyril Mac <thari69@gmail.com>
2017-11-21 10:20:45 +0530

    V1

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-       /* academic test = new academic();
+        academic test = new academic();
         course COSC22035 = new course();
         test.name="Liyanage";
         COSC22035.creditValue=5;
         test.salary=333333;
         test.courseTaught=COSC22035;
         test.outSal();
-        test.calcSal();*/
-
-        Light p = new Light();
-        LEDLight m = new LEDLight();
-
-/*Which of the following operations is a valid operation?*/
-
-        p.turnOn(10);
-        p.make = "GT";
-        p.power = 200;
-        m.turnOn(20);
+        test.calcSal();
 
 
     }
