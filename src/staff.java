9acb82efaae3379c0231e2122b1bbeff020dcd3f
Cyril Mac <thari69@gmail.com>
2017-11-21 09:50:31 +0530

    V1

--- src/staff.java
+++ src/staff.java
@@ -1,5 +1,4 @@
 public class staff extends user {
-    //Constructor for staff
     String universityID;
     double salary;
     String department;
@@ -11,13 +10,7 @@ public class staff extends user {
         if (this.email!=null) System.out.println("email: "+this.email);
         System.out.println("Phone Number: "+this.phoneNumber);
     }
-/*
-    private staff (String inUID,String inDept)
-    {
-        universityID = inUID;
-        department = inDept;
-    }
-*/
+
     void SalIncrement(){
         if(this.department.equals("Computer Science")) this.salary=this.salary*1.05;
     }
