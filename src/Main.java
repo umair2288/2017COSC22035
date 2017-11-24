ba40b72396f76d4212d9b10f83e7c19e106fdf0e
Cyril Mac <thari69@gmail.com>
2017-11-24 10:31:27 +0530

    V1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,41 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        academic test = new academic();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input Lecturer Name:");
+        academic UPL = new academic();
         course COSC22035 = new course();
-        test.name="Liyanage";
-        COSC22035.creditValue=5;
-        test.salary=333333;
-        test.courseTaught=COSC22035;
-        test.outSal();
-        test.calcSal();
+        UPL.name = sc.nextLine();
+        System.out.println("Insert Phone Number:");
+        UPL.phoneNumber = sc.nextInt();
+        UPL.displayInfo();
+        /*
+        try{
+            System.out.println(COSC22035.creditValue/0);
+        }
+        catch (ArithmeticException exp)
+        {
+            exp.printStackTrace();
+        }
+        try{
+            System.out.println(args[0]);
+        }
+        catch (ArrayIndexOutOfBoundsException exp)
+        {
+            exp.printStackTrace();
+        }
+        */
+        //test.name=sc.nextLine();
+
+        //System.out.println("Out of Exception");
+        //System.out.print("Input Credit Value:");
+        //COSC22035.creditValue=sc.nextInt();
+        UPL.salary=333333;
+        UPL.courseTaught=COSC22035;
+        UPL.outSal();
+        UPL.calcSal();
+
+
     }
 }
